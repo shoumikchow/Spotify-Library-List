673dc0c8647cf097cebc7778a26b2323c38d0eac
Shoumik <shoumikchow@gmail.com>
2019-11-17 12:33:03 -0600

    Added argparse options

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 This is just a simple script for saving the list of all songs in the Spotify library to a CSV file. 
 Need to get Client ID and Client Secret from https://developer.spotify.com/dashboard/
+
+### Install Dependecies
+> pip3 install -r requirements.txt
+
+### Usage
+> python3 spotify.py -id &lt;client-id&gt; -secret &lt;client-secret&gt;
+
+OR 
+
+> python3 spotify.py --client_id &lt;client-id&gt; --client_secret &lt;client-secret&gt;
--- spotify.py
+++ spotify.py
@@ -1,14 +1,31 @@
-import spotipy
-import spotipy.util as util
+import argparse
 import csv
 import math
-from tqdm import tqdm
 import operator
+
+from tqdm import tqdm
+
+import spotipy
+import spotipy.util as util
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-id',
+                    '--client_id',
+                    help='enter client id from developer.spotify.com')
+parser.add_argument('-secret',
+                    '--client_secret',
+                    help='enter client secret from developer.spotify.com')
 
 scope = 'user-library-read'
 username = 'shoumikchow'
 
-token = util.prompt_for_user_token(username, scope, client_id='client_id', client_secret='client_secret', redirect_uri='http://localhost/')
+args = parser.parse_args()
+
+token = util.prompt_for_user_token(username,
+                                   scope,
+                                   client_id=args.client_id,
+                                   client_secret=args.client_secret,
+                                   redirect_uri='http://localhost/')
 
 total = int(input("Enter total number of songs in your library: "))
 
@@ -18,7 +35,10 @@ for i in tqdm(range(math.ceil(total / 20))):
     results = sp.current_user_saved_tracks(offset=20 * i)
     for item in results['items']:
         track = item['track']
-        songs_artists_albums.append([track['name'], track['artists'][0]['name'], track['album']['name'], track['external_urls']['spotify']])
+        songs_artists_albums.append([
+            track['name'], track['artists'][0]['name'], track['album']['name'],
+            track['external_urls']['spotify']
+        ])
 
 sortedlist = sorted(songs_artists_albums, key=operator.itemgetter(1))
 
